5fc00ea04c0979a68e442bdca4197d8d5c36096e
Nadja Stauber <nadja.stauber@yahoo.de>
2025-09-29 21:34:30 +0200

    getOne via id hinzugefuegt, Fehlermeldung funktioniert noch nicht

--- routes.js
+++ routes.js
@@ -20,5 +20,25 @@ router.post('/tasks', async (req, res) => {
     res.send(newTask);
 });
 
+// get one task via id 
+// :name des Parameters (hat Bedeutung), wird wieder aus request Objekt ausgelesen
+
+router.get('/tasks/:id', async (req, res) => {
+    
+    //findOne gibt null zurück (vs. find gibt Array)
+    let task = await Task.findOne({ _id: req.params.id }); // Objekte in der Datenbank suchen, die mit der id übereinstimmen
+
+    if (task) {
+        res.send(task);
+    }
+    else {
+        res.status(404);
+        res.send({
+            error: "Task does not exist!"       // ich komme nicht zur Fehlermeldung, sondern Exception wird geworfen??
+        });
+    }
+})
+
+
 
 module.exports = router;
