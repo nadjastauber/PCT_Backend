a3f9649eb43f8cd5d84d8cf65093d8085a3b1bd7
Nadja Stauber <nadja.stauber@yahoo.de>
2025-09-30 16:19:18 +0200

    update und delete Endpunkte erstellt

--- models/tasks.js
+++ models/tasks.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
+    status: String,
     name: String,
     date: String
 });
--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const router = express.Router(); //Router leitet Request an passende Route weite
 const Task = require('./models/tasks');
 
 
+
 // get all tasks
 router.get('/tasks', async (req, res) => {
     const allTasks = await Task.find();
@@ -11,8 +12,10 @@ router.get('/tasks', async (req, res) => {
 });
 
 // post one task
+//request body enthält infos??
 router.post('/tasks', async (req, res) => {
     const newTask = new Task({
+        status: req.body.status,
         name: req.body.name,
         date: req.body.date,
     })
@@ -22,23 +25,55 @@ router.post('/tasks', async (req, res) => {
 
 // get one task via id 
 // :name des Parameters (hat Bedeutung), wird wieder aus request Objekt ausgelesen
+// request params bezieht sich auf letzten Abschnitt URL?
 
 router.get('/tasks/:id', async (req, res) => {
     
-    //findOne gibt null zurück (vs. find gibt Array)
-    let task = await Task.findOne({ _id: req.params.id }); // Objekte in der Datenbank suchen, die mit der id übereinstimmen
-
-    if (task) {
+    //findOne gibt Objekt oder null zurück (vs. find gibt Array zurück)
+    try{
+        let task = await Task.find({ _id: req.params.id }); // Objekte in der Datenbank suchen, die mit der id übereinstimmen
         res.send(task);
     }
-    else {
-        res.status(404);
+
+    catch(err){
+        res.status(404)
         res.send({
-            error: "Task does not exist!"       // ich komme nicht zur Fehlermeldung, sondern Exception wird geworfen??
+            error: "Task does not exist!"       // falls kein Task mit id gefunden, Fehlermdeldung
         });
+
     }
 })
 
+// update one task
+router.patch('/tasks/:id', async(req, res) => {
+    try {
+        const task = await Task.findOne({ _id: req.params.id })
 
+        if (req.body.name) {
+            task.name = req.body.name
+        }
 
-module.exports = router;
+        if (req.body.date) {
+            task.date = req.body.date
+        }
+
+        await Task.updateOne({ _id: req.params.id }, task);     //updateOne ändert und sendet id zurück (sehe ich das?)
+        res.send(task)
+    } catch {                           //brauche ich den zweiten Teil, ich debe die Ids ja eig nicht ein??
+       res.status(404)
+        res.send({ error: "Task does not exist!" })
+    }
+});
+
+// delete one task via id
+router.delete('/tasks/:id', async(req, res) => {
+    try {
+        await Task.deleteOne({ _id: req.params.id })
+        res.status(204).send()
+    } catch {
+        res.status(404)
+        res.send({ error: "Task does not exist!" })
+    }
+});
+
+module.exports = router; //damit router überall in Projekt genutzt werden kann (alle Endpunkte genutzt werden können)
